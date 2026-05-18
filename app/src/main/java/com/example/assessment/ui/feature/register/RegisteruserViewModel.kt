4ca16004093c244f815b51701ed9a696e45b92c8
sharan <sharanparmar15@gmail.com>
2026-05-18 19:22:14 +0530

    code refactor

--- app/src/main/java/com/example/assessment/ui/feature/register/RegisteruserViewModel.kt
+++ app/src/main/java/com/example/assessment/ui/feature/register/RegisteruserViewModel.kt
@@ -3,15 +3,13 @@ package com.example.assessment.ui.feature.register
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.assessment.data.model.CustomerRequest
+import com.example.assessment.data.network.ApiResult
 import com.example.assessment.data.repository.RegisterUserRepository
 import com.example.assessment.ui.state.UIState
-import com.example.assessment.data.network.ApiResult
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
